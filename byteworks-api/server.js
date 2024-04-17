a0224698ccc2d8cc6cc42894dcb65c12af6cb8c0
Sovalle93 <sovalleg93@gmail.com>
2024-04-17 08:02:37 -0400

    changing server

--- byteworks-api/server.js
+++ byteworks-api/server.js
@@ -1,7 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { logger } from "logger-express";
-
 
 import registerBusinessRoutes from "./config/routes/registerBusinessRoutes.js";
 import registerUserRoutes from "./config/routes/registerUserRoutes.js";
@@ -18,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use(logger());
+
 
 app.use("/", registerUserRoutes);
 app.use("/", loginRoutes);
